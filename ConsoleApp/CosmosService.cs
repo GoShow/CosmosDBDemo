f0e8d4affe6c83f9ad28d28ce27f9bdd8e7b994f
Georgi Inkov <georgi.inkov@scaleforce.io>
2022-03-29 11:02:11 +0300

    Refactoring and update presentation

--- ConsoleApp/CosmosService.cs
+++ ConsoleApp/CosmosService.cs
@@ -14,10 +14,6 @@ namespace ConsoleApp;
 
 public class CosmosService
 {
-    //private const string EndpointUri = "<YOUR URI>";
-    //private const string PrimaryKey = "<YOUR PRIMARY KEY>";
-
-    //Local Settings:
     private const string EndpointUri = "https://localhost:8081";
     private const string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
